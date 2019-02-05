53f5d5f99a4f9209e4984dade705238e64affb27
theesue <46975971+theesue@users.noreply.github.com>
2019-02-04 17:14:28 -0800

    Add files via upload

--- assets/js/email_handler.js
+++ assets/js/email_handler.js
@@ -1,24 +1,28 @@
-var form = document.querySelector("#contact-form");
+document.onload = () => {
+    var form = document.querySelector("#contact-form");
+    var button = document.querySelector('#contact-form-send');
 
-form.submit(function(event) {
-    event.preventDefault();
-
-    var template_params = {
-        'sender_email' : 'send_email_value',
-        'full_name' : 'full_name_value',
-        'message' : 'message_value'
-    };
-
-    var service_id = 'gmail';
-    var template_id = 'hmong_translator';
-
-    form.querySelector('#contact-form-send').innerHTML = 'Sending...';
-    emailjs.send(service_id, template_id, template_params)
-        .then(function(response) {
-            console.log('successs', reponse.status, reponse.text);
-        }, function(error) {
-            console.log('failed', error);
-            form.querySelector('#contact-form-send').innerHTML = 'Send';
-        });
-        return false;
-});
+    form.submit(function(event) {
+        event.preventDefault();
+    
+        var template_params = {
+            'sender_email' : 'send_email_value',
+            'full_name' : 'full_name_value',
+            'message' : 'message_value'
+        }
+    
+        var service_id = 'gmail';
+        var template_id = 'hmong_translator';
+    
+        button.innerHTML = 'Sending...';
+        emailjs.send(service_id,template_id,template_params)
+            .then(function(response) {
+                console.log('successs', reponse.status, reponse.text);
+                button.innerHTML = 'Send';
+            }, function(err) {
+                console.log('failed', err);
+                button.innerHTML = 'Send';
+            });
+            return false;
+    });   
+};
